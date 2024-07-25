b7510cf6eed4ff57a4cf9b0a839dca8cb260d0cc
behzadnabawi <behzadnabawi@gmail.com>
2024-07-25 14:36:16 +0330

    Add phoneOrderPayment

--- Exp_01_Non_SOLID/PaymentService/PhoneOrderService.java
+++ Exp_01_Non_SOLID/PaymentService/PhoneOrderService.java
@@ -25,4 +25,9 @@ public class PhoneOrderService implements OrderService{
     public void phoneOrderRegister(String customerName) {
         System.out.println("phone order registered for " + customerName);
     }
+
+    @Override
+    public void phoneOrderPayment(int foodPrice) {
+        System.out.println("phone Payment with Price : " + foodPrice + " Tomans!");
+    }
 }
